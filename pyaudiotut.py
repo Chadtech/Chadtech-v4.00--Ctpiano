8772702eb014d7e2ac969adcb3fb2a5538ae8a34
ChadTech <chadstearns1@gmail.com>
2014-05-05 20:34:15 -0700

    Fooled around a lot with pyaudio

--- pyaudiotut.py
+++ pyaudiotut.py
@@ -20,57 +20,62 @@ welcomescreen = pygame.image.load('welcomescreen.PNG').convert()
 p = pyaudio.PyAudio()
 stream = p.open(rate=44100, channels=1, format=pyaudio.paFloat32, output=True)
 
-for yit in range(pygame.midi.get_count() ):
-	print pygame.midi.get_device_info(yit), yit
+def sine_wave(frequency=440.0, framerate=44100, amplitude=0.5):
+    period = int(framerate / frequency)
+    if amplitude > 1.0: amplitude = 1.0
+    if amplitude < 0.0: amplitude = 0.0
+    lookup_table = [float(amplitude) * math.sin(2.0*math.pi*float(frequency)*(float(i%period)/float(framerate))) for i in xrange(period)]
+    return (lookup_table[i%period] for i in range(period))
 
-inp = pygame.midi.Input(0)
+ONon = sine_wave(400)
 
 tones=[]
 os.chdir(os.path.abspath('JIT Europe Sines'))
 for yit in os.listdir(os.getcwd()):
 	if  yit.endswith('.wav'):
 		tones.append(pygame.mixer.Sound(yit))
-
+toneFire = False
+countDown = False
+phasePosition = 0
+volume = 100.
 goin = True
 while goin:
 
 	screen.blit(welcomescreen,[0,0])
 
 	for event in pygame.event.get():
-		#if event.type == pygame.KEYDOWN:
-		#	if event.key == pygame.K_a:
-		#		stream.write(array.array('f',(.25 * math.sin(i / 10.) for i in range(1000))).tostring())
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				toneFire=True
 
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				countDown=True
+				phasePosition=0
 
 		if event.type == pygame.QUIT:
 			goin = False
+
+	if toneFire==True:
+		if countDown==False:
+			stream.write(array.array('f',(yit for yit in ONon)).tostring())
+		else:
+			stream.write(array.array('f',sine_wave(400, amplitude=volume/200.)).tostring())
+			print volume
+			volume-=3
+			if volume<0:
+				toneFire=False
+				countDown=False
+				volume=100.
 
 ###### [[(144 if its a down press, 128 if its up), Midi note, (velocity, where 127 is loudest. Always 127 when up press) ], Time of event]
 ###### 0 is the bottom midi note (C), 120 is the top (if)
 
 ###### Gonna make three scales
 
-	if inp.poll():
-		keys = inp.read(1000)
-		for keyPressed in keys:
-			pressData = keyPressed[0]
-			print pressData
-			pressDirection = pressData[0]
-			midiNumber = pressData[1]
-			velocity = pressData[2]
-			if pressDirection==144:
-				tones[midiNumber].set_volume(velocity/127.)
-				tones[midiNumber].play()
-				tones[midiNumber].fadeout(int(5000*(velocity/127.)))
-			elif pressDirection==128:
-				tones[midiNumber].stop()
-				tones[midiNumber].fadeout(30)
-
 	pygame.display.flip()
 	clock.tick(44100)
 
 pygame.quit()
-#stream.close()
-#p.terminate()
+stream.close()#p.terminate()
 
-	
